422dc33af0f1121e5e1369ba90777a8a98332a81
Praful-Tulaskar <114746088+Praful-Tulaskar@users.noreply.github.com>
2023-05-30 18:08:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import News from './components/News'
+import NavBar from './components/NavBar'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <News/>
+      </div>
+    )
+  }
+}
